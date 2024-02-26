bab8396ee77ea84062e64ebc9ae3d6706d8ccccd
Sergei Mineev <msergey95@gmail.com>
2024-02-26 13:20:43 +0800

    Initial commit

---
+++ Navigation/ProfileViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ProfileViewController.swift
+//  Navigation
+//
+//  Created by Сергей Минеев on 2/23/24.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .green
+    }
+}
