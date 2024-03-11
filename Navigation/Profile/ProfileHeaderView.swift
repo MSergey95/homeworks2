b076fb162eddde9fba341d76870d677d2f3cae2a
Sergey <121692119+MSergey95@users.noreply.github.com>
2024-03-12 00:24:04 +0800

    Add new functions for Profile

---
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -0,0 +1,93 @@
+//
+//  ProfileHeaderView.swift
+//  Navigation
+//
+//  Created by Сергей Минеев on 3/11/24.
+//
+
+import UIKit
+class ProfileHeaderView: UIView {
+    
+    let avatarImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.layer.cornerRadius = 35
+        imageView.clipsToBounds = true
+        imageView.layer.borderWidth = 3
+        imageView.layer.borderColor = UIColor.black.cgColor
+        imageView.image = UIImage(named: "catImage") 
+        return imageView
+    }()
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Hipster Cat"
+        label.font = UIFont.boldSystemFont(ofSize: 18)
+        label.textColor = .black
+        return label
+    }()
+    
+    let statusButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .systemBlue
+        button.setTitle("Show Status", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.shadowOffset = CGSize(width: 4, height: 4)
+        button.layer.shadowRadius = 4
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOpacity = 0.7
+        button.layer.cornerRadius = 10 // Добавил скругление углов
+        return button
+    }()
+    
+    let waitingLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Waiting for something..."
+        label.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        label.textColor = .gray
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(avatarImageView)
+        addSubview(nameLabel)
+        addSubview(statusButton)
+        addSubview(waitingLabel)
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            avatarImageView.topAnchor.constraint(equalTo: self.topAnchor, constant: 27),
+            avatarImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
+            avatarImageView.widthAnchor.constraint(equalToConstant: 70),
+            avatarImageView.heightAnchor.constraint(equalToConstant: 70),
+        ])
+        
+        NSLayoutConstraint.activate([
+            nameLabel.centerYAnchor.constraint(equalTo: avatarImageView.centerYAnchor),
+            nameLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 16),
+            nameLabel.trailingAnchor.constraint(lessThanOrEqualTo: self.trailingAnchor, constant: -16),
+        ])
+        
+        NSLayoutConstraint.activate([
+            statusButton.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 16),
+            statusButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -16),
+            statusButton.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -16), // Для размещения внизу
+            statusButton.heightAnchor.constraint(equalToConstant: 50),
+        ])
+        
+        NSLayoutConstraint.activate([
+            waitingLabel.bottomAnchor.constraint(equalTo: statusButton.topAnchor, constant: -34),
+            waitingLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+        ])
+    }
+}
